aec05fb8d0ab81b3cc425c727da80aa5a1bcc10c
pawnlord <scruffduff321@gmail.com>
2020-07-17 15:48:35 -0400

    Add movement using normal notation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 #include <Board.hpp>
 #include <sstream>
 
+int letterToNumber(char letter){
+    if(letter <= 'Z'){
+        return letter-'A';
+    } else if(letter <= 'z'){
+        return letter-'a';
+    } else {
+        std::cout << "Error: No letter " << (int)letter << std::endl;
+        return -1;
+    }
+    
+}
+
 int main(){
     std::vector<std::vector<char> >  board = {
         {'R','k','B','Q','K','B','k','R'},
@@ -19,18 +31,21 @@ int main(){
     std::string input = "";
     while(input != "exit"){
         b.draw();
-        std::cout << "x: ";
+        std::cout << "Your Move: ";
         std::cin >> input;
-        int from_x = std::stoi(input);
-        std::cout << "y: ";
-        std::cin >> input;
-        int from_y = std::stoi(input);
-        std::cout << "x: ";
-        std::cin >> input;
-        int to_x = std::stoi(input);
-        std::cout << "y: ";
-        std::cin >> input;
-        int to_y = std::stoi(input);
-        b.move(from_x, from_y, to_x, to_y);
+        if(input.size() < 5){
+            std::cout << "Error: not long enough (movement syntax: <NUMBER><LETTER>-<NUMBER2><LETTER2>)" << std::endl;
+        } else if(input[0] < '1' || input[0] > '8' || input[3] < '1' || input[3] > '8'){
+            std::cout << "Error: number needed for position (movement syntax: <NUMBER><LETTER>-<NUMBER2><LETTER2>)"<< std::endl;
+        } else if(input[1] < 'A' || ((input[1] > 'H' && input[1] < 'a') || input[1] > 'h') 
+                || input[4] < 'H' || ((input[4] > 'H' && input[4] < 'a') || input[4] > 'h') ){
+            std::cout << "Error: letter (a-h) needed for position (movement syntax: <NUMBER><LETTER>-<NUMBER2><LETTER2>)"<< std::endl;
+        } else if(input[2] != '-'){
+            std::cout << "Error: dash needed for position (movement syntax: <NUMBER><LETTER>-<NUMBER2><LETTER2>)" << std::endl;
+        } else {
+            int from_x = input[0]-'1', from_y = letterToNumber(input[1]);
+            int to_x = input[3]-'1', to_y = letterToNumber(input[4]);
+            b.move(from_x, from_y, to_x, to_y);
+        }
     }
 }
